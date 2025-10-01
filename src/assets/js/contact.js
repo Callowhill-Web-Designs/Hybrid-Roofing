854e4bb1a1359bace6e9136afcbb7cf57a7cb181
Zade Dohan <105446965+zade0123@users.noreply.github.com>
2025-10-01 18:01:28 -0400

    State restriction and netlify form fix

--- src/assets/js/contact.js
+++ src/assets/js/contact.js
@@ -7,6 +7,7 @@ class ContactForm {
     constructor() {
         this.form = null;
         this.isSubmitting = false;
+        this.allowedStates = ['PA', 'MD', 'DE', 'NJ'];
         this.init();
     }
 
@@ -82,55 +83,9 @@ class ContactForm {
 
         const states = [
             { code: 'PA', name: 'Pennsylvania', priority: true },
-            { code: 'AL', name: 'Alabama' },
-            { code: 'AK', name: 'Alaska' },
-            { code: 'AZ', name: 'Arizona' },
-            { code: 'AR', name: 'Arkansas' },
-            { code: 'CA', name: 'California' },
-            { code: 'CO', name: 'Colorado' },
-            { code: 'CT', name: 'Connecticut' },
-            { code: 'DE', name: 'Delaware' },
-            { code: 'FL', name: 'Florida' },
-            { code: 'GA', name: 'Georgia' },
-            { code: 'HI', name: 'Hawaii' },
-            { code: 'ID', name: 'Idaho' },
-            { code: 'IL', name: 'Illinois' },
-            { code: 'IN', name: 'Indiana' },
-            { code: 'IA', name: 'Iowa' },
-            { code: 'KS', name: 'Kansas' },
-            { code: 'KY', name: 'Kentucky' },
-            { code: 'LA', name: 'Louisiana' },
-            { code: 'ME', name: 'Maine' },
             { code: 'MD', name: 'Maryland' },
-            { code: 'MA', name: 'Massachusetts' },
-            { code: 'MI', name: 'Michigan' },
-            { code: 'MN', name: 'Minnesota' },
-            { code: 'MS', name: 'Mississippi' },
-            { code: 'MO', name: 'Missouri' },
-            { code: 'MT', name: 'Montana' },
-            { code: 'NE', name: 'Nebraska' },
-            { code: 'NV', name: 'Nevada' },
-            { code: 'NH', name: 'New Hampshire' },
-            { code: 'NJ', name: 'New Jersey' },
-            { code: 'NM', name: 'New Mexico' },
-            { code: 'NY', name: 'New York' },
-            { code: 'NC', name: 'North Carolina' },
-            { code: 'ND', name: 'North Dakota' },
-            { code: 'OH', name: 'Ohio' },
-            { code: 'OK', name: 'Oklahoma' },
-            { code: 'OR', name: 'Oregon' },
-            { code: 'RI', name: 'Rhode Island' },
-            { code: 'SC', name: 'South Carolina' },
-            { code: 'SD', name: 'South Dakota' },
-            { code: 'TN', name: 'Tennessee' },
-            { code: 'TX', name: 'Texas' },
-            { code: 'UT', name: 'Utah' },
-            { code: 'VT', name: 'Vermont' },
-            { code: 'VA', name: 'Virginia' },
-            { code: 'WA', name: 'Washington' },
-            { code: 'WV', name: 'West Virginia' },
-            { code: 'WI', name: 'Wisconsin' },
-            { code: 'WY', name: 'Wyoming' }
+            { code: 'DE', name: 'Delaware' },
+            { code: 'NJ', name: 'New Jersey' }
         ];
 
         this.createStateDropdown(stateInput, states);
@@ -287,10 +242,26 @@ class ContactForm {
     }
 
     validateState(value) {
-        const validStates = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
+        const serviceStates = ['PA', 'MD', 'DE', 'NJ'];
+        const upperValue = value.toUpperCase();
+        
+        if (!upperValue) {
+            return {
+                isValid: false,
+                message: 'Please select a state'
+            };
+        }
+        
+        if (!serviceStates.includes(upperValue)) {
+            return {
+                isValid: false,
+                message: 'Sorry, Hybrid Roofing PA does not operate in your area. We serve PA, MD, DE, and NJ.'
+            };
+        }
+        
         return {
-            isValid: validStates.includes(value.toUpperCase()),
-            message: 'Please enter a valid state abbreviation (e.g., PA)'
+            isValid: true,
+            message: ''
         };
     }
 
